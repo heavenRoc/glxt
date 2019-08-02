486140b32d72d98b91c11aaf6b9f971c4c5b2aa9
zhaojiepeng <zhaojiepeng@thunisoft.com>
2019-08-02 18:34:08 +0800

    【*】

--- 文档/sql/gym.sql
+++ 文档/sql/gym.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50719
 File Encoding         : 65001
 
-Date: 2019-07-24 15:18:31
+Date: 2019-08-02 18:33:34
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,7 +29,8 @@ CREATE TABLE `t_account` (
 -- ----------------------------
 -- Records of t_account
 -- ----------------------------
-INSERT INTO `t_account` VALUES ('1', 'sa', 'sa', '1');
+INSERT INTO `t_account` VALUES ('35850cd7204b4998a59bb2227b4c6d02', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'be62393e65394d5ab90593e56b3656cf');
+INSERT INTO `t_account` VALUES ('67EA792C2C194EC8863497CB9B8268B9', 'zhangsan', 'E10ADC3949BA59ABBE56E057F20F883E', '6A5B7283B848459EB186AEB922F7B721');
 
 -- ----------------------------
 -- Table structure for t_coach
@@ -48,7 +49,8 @@ CREATE TABLE `t_coach` (
 -- ----------------------------
 -- Records of t_coach
 -- ----------------------------
-INSERT INTO `t_coach` VALUES ('1', '王二', '男', '19', '111', null, '1');
+INSERT INTO `t_coach` VALUES ('be62393e65394d5ab90593e56b3656cf', '管理员', '男', '19', '111', 'https://i.loli.net/2019/07/24/5d3806a1ae47179446.jpg', '1');
+INSERT INTO `t_coach` VALUES ('6A5B7283B848459EB186AEB922F7B721', '张三', '男', '20', '18395589678', 'https://i.loli.net/2019/05/22/5ce43019c4ba860403.png', '2');
 
 -- ----------------------------
 -- Table structure for t_course
@@ -62,6 +64,7 @@ CREATE TABLE `t_course` (
 -- ----------------------------
 -- Records of t_course
 -- ----------------------------
+INSERT INTO `t_course` VALUES ('850058331DA047C191F56992B1F128C1', '举重');
 
 -- ----------------------------
 -- Table structure for t_record
@@ -95,6 +98,10 @@ CREATE TABLE `t_user` (
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
+INSERT INTO `t_user` VALUES ('D3F46E412F0A40609442F2649EC95A71', '1', '男', '1', '1');
+INSERT INTO `t_user` VALUES ('33EFDF61660D493FBAB9813A8278EB67', '1', '男', '1', '1');
+INSERT INTO `t_user` VALUES ('924E629D14294E06A7C07D80A966484F', '1', '男', '1', '1');
+INSERT INTO `t_user` VALUES ('FF41429361964BA48E8B09D769AF9BC5', '1', '男', '1', '1');
 
 -- ----------------------------
 -- Table structure for t_user_ks
@@ -106,9 +113,11 @@ CREATE TABLE `t_user_ks` (
   `c_course_bh` varchar(255) DEFAULT NULL,
   `c_sysks` varchar(255) DEFAULT NULL,
   `c_ksjg` varchar(255) DEFAULT NULL,
-  `dt_cjsj` varchar(255) DEFAULT NULL
+  `dt_cjsj` varchar(255) DEFAULT NULL,
+  `c_coach_bh` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_user_ks
 -- ----------------------------
+INSERT INTO `t_user_ks` VALUES ('F7AD108C20ED41E199F843F4A97E49C3', 'FF41429361964BA48E8B09D769AF9BC5', '850058331DA047C191F56992B1F128C1', '1', '1', '2019-08-02 17:04:39', '6A5B7283B848459EB186AEB922F7B721');
